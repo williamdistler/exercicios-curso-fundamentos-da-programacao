0d6f1eeda38bb1315a293c32c0cace85d6184b89
William Distler Neves <william.distler@outlook.com>
2024-02-27 01:05:58 -0300

    First Commit

---
+++ ola-mundo.js
@@ -0,0 +1,14 @@
+/**
+ * Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+ * função) e com ponto de exclamação "!" no final.
+ * Exemplos:
+ * cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+ * cumprimentar("Maria") // retornará "Olá, Maria!"
+ */
+
+function cumprimentar(name) {
+  return 'Olá, ' + name + '!'
+}
+
+console.log('Leonardo')
+console.log('Maria')
